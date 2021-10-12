c3a106c80f2b070f78a3f303402492e34edd1ab6
xUborka <xgurke@gmail.com>
2021-10-12 22:21:22 +0200

    PowerBar POC implemented

--- Assets/Scripts/PowerBarScript.cs
+++ Assets/Scripts/PowerBarScript.cs
@@ -1,49 +1,57 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PowerBarScript : MonoBehaviour
 {
     [SerializeField] private GameObject circles;
-    [SerializeField] private GameObject my_start;
-    [SerializeField] private GameObject my_mid;
-    [SerializeField] private GameObject my_end;
+    [SerializeField] private GameObject projectiles;
+    [SerializeField] private GameObject top_bar;
+    [SerializeField] private Slider slider;
     [SerializeField] private GameObject my_text;
-    [SerializeField] private GameObject en_start;
-    [SerializeField] private GameObject en_mid;
-    [SerializeField] private GameObject en_end;
     [SerializeField] private GameObject en_text;
-    [SerializeField] private GameObject npc_mid;
-    [SerializeField] private GameObject npc_text;
+
     // Start is called before the first frame update
     void Start()
     {
-        npc_text.GetComponent<TextMeshProUGUI>().SetText($"1");
-        npc_text.transform.position = Camera.main.WorldToScreenPoint(npc_mid.transform.position);
+        print(my_text.transform.localPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
         float mine = 0.0f;
-        float npc = 0.0f;
         float enemy = 0.0f;
         for (int i = 0; i < circles.transform.childCount; ++i){
             Spawner current_circle = circles.transform.GetChild(i).gameObject.GetComponent<Spawner>();
-            if (current_circle.Owner == 0){
-                npc += current_circle.Spawned_units;
-            } else if (current_circle.Owner == 1){
+            if (current_circle.Owner == 1){
                 mine += current_circle.Spawned_units;
             } else if (current_circle.Owner == 2){
                 enemy += current_circle.Spawned_units;
             }
         }
-        float sum = mine + npc + enemy;
-        mine /= sum;
-        npc /= sum;
-        enemy /= sum;
-        my_mid.transform.localScale = new Vector3(mine * 7.4f, 1.0f, 1.0f);
-        npc_text.transform.position = Camera.main.WorldToScreenPoint(npc_mid.transform.position);
-        en_text.transform.position = Camera.main.WorldToScreenPoint(en_mid.transform.position);
-        my_text.transform.position = Camera.main.WorldToScreenPoint(my_mid.transform.position);
+        for (int i = 0; i < projectiles.transform.childCount; ++i){
+            ProjectileScript current_projectile = projectiles.transform.GetChild(i).gameObject.GetComponent<ProjectileScript>();
+            if (current_projectile.owner == 1){
+                mine += current_projectile.units;
+            } else if (current_projectile.owner == 2){
+                enemy += current_projectile.units;
+            }
+        }
+        my_text.GetComponent<TextMeshProUGUI>().SetText($"{mine}");
+        en_text.GetComponent<TextMeshProUGUI>().SetText($"{enemy}");
+        mine /= mine + enemy;
+        
+        if (mine < 0.15f){
+            my_text.GetComponent<TextMeshProUGUI>().SetText("");
+        } else if (mine > 0.85f){
+            en_text.GetComponent<TextMeshProUGUI>().SetText("");
+        }
+
+        slider.value = mine;
+        mine -= 0.5f;
+        float width = GetComponent<RectTransform>().rect.width;
+        my_text.transform.localPosition = new Vector3(Mathf.Lerp(-0.5f*width, mine*width, 0.5f), 0f, 0);
+        en_text.transform.localPosition = new Vector3(Mathf.Lerp(mine*width, 0.5f*width, 0.5f), 0f, 0);
     }
 }
